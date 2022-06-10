b989ab288218f3837d12d0395725cca9ead02dfb
Danton de Lima <dantondelima.contato@gmail.com>
2022-06-10 18:42:21 -0300

    Perfil bottom navigation

---
+++ perfil_bottom_nav/lib/pages/Experiencia.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+ 
+class Experiencia extends StatelessWidget {
+  const Experiencia({Key? key}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body(context),
+    );
+  }
+ 
+  _body(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 100, bottom: 20),
+      child: ListView(
+        children: <Widget>[
+          _descricao(
+            '- Dev Jr. KBRTEC',
+            '- Dev Pleno Yampi'        
+          ),
+        ],
+      ),
+    );
+  }
+
+  _descricao(titulo, descricao1) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _texto(titulo),
+            _texto(descricao1),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _texto(texto) {
+    return Text(
+      texto,
+      style: TextStyle(
+          color: Colors.blue,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic,
+          decoration: TextDecoration.underline,
+          decorationStyle: TextDecorationStyle.wavy),
+    );
+  }
+}
